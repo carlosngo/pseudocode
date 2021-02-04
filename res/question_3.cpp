983a4c942e41fb89ec5b3dcacbfa1ad07ffdee1b
Administrator <kathleen@zayls.com>
2021-02-04 12:15:42 +0800

    fix q 3

--- res/question_3.cpp
+++ res/question_3.cpp
@@ -1,6 +1,5 @@
 main()
 {
-    
     int[] arr = create int[5];
     for int i = 0 up to 4 {
         int temp;
@@ -11,6 +10,4 @@ main()
     for int i = 4 down to 0 {
         print(arr[i]);
     }
-
-    return 0;
 }
